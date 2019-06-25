0e40f19bd9b182c70f69f134be504df32596562d
malovevgen <malovevgen@yandex.ru>
2019-06-25 22:03:51 +0300

    folder is deleted

--- projects/black_jak/dealer.rb
+++
@@ -1,8 +0,0 @@
-class Dealer
-  attr_accessor :cards, :points
-
-  def initialize
-    @cards = []
-    @points = 0
-  end
-end
--- projects/black_jak/main.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'controller'
-require_relative 'bank'
-require_relative 'cards'
-require_relative 'dealer'
-require_relative 'user'
-
-game = Controller.new
-game.start_game
